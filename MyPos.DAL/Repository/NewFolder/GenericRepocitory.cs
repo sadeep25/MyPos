6bbd4ed4b258a01f722182dbe56527a0a67d8fd0
sadeep <sadeeproshan22@gmail.com>
2018-05-03 13:41:34 +0530

    New Models Done

---
+++ MyPos.DAL/Repository/NewFolder/GenericRepocitory.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPos.DAL.Repository.NewFolder
+{
+    public class GenericRepocitory<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly DbContext context = null;
+        private readonly DbSet<TEntity> dbSet = null;
+
+        public GenericRepocitory(DbContext context)
+        {
+            this.context = context;
+            dbSet = context.Set<TEntity>();
+        }
+
+        public void Delete(object id)
+        {
+            TEntity entityToDelete = dbSet.Find(id);
+            Delete(entityToDelete);
+        }
+
+        public void Delete(TEntity entityToDelete)
+        {
+            if (context.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                dbSet.Attach(entityToDelete);
+            }
+            dbSet.Remove(entityToDelete);
+        }
+
+        public IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "")
+        {
+            IQueryable<TEntity> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            foreach (var includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            else
+            {
+                return query.ToList();
+            }
+        }
+
+        public TEntity GetByID(object id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public void Insert(TEntity entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public void Update(TEntity entityToUpdate)
+        {
+            dbSet.Attach(entityToUpdate);
+            context.Entry(entityToUpdate).State = EntityState.Modified;
+        }
+    }
+}
---
+++ MyPos.DAL/Repository/NewFolder/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using MyPos.DAL.Context;
+using MyPos.DAL.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPos.DAL.Repository.NewFolder
+{
+    public interface IUnitOfWork
+    {
+         MyPosDbContext DbContext { get; }
+
+        GenericRepository<Customer> CustomerRepository { get; }
+        GenericRepository<Order> OrderRepository { get; }
+        GenericRepository<Product> ProductRepository { get; }
+        GenericRepository<OrderItem> OrderItemRepository { get; }
+
+        void Save();
+    }
+}
